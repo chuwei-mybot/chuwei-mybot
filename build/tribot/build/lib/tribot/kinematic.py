baa52e18f5ed0ff4d181831b3e3c058e69ab5e57
Aries-441 <sjtu.liu.jj@gmail.com>
2022-11-13 11:44:07 +0800

    date_22_11_12

---
+++ build/tribot/build/lib/tribot/kinematic.py
@@ -0,0 +1,135 @@
+'''
+FileName: 
+Description: 
+Autor: Liujunjie/Aries-441
+StudentNumber: 521021911059
+Date: 2022-11-10 19:20:01
+E-mail: sjtu.liu.jj@gmail.com/sjtu.1518228705@sjtu.edu.cn
+LastEditTime: 2022-11-11 23:24:27
+'''
+
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import rclpy
+from rclpy.node import Node
+from rclpy import time
+import numpy as np
+from gazebo_msgs.msg import ModelState
+from geometry_msgs.msg import Twist, TransformStamped
+from tf2_ros.static_transform_broadcaster import StaticTransformBroadcaster 
+# from nav_msgs.msg import Odometry
+from math import sin,cos,atan2
+import tf
+
+init_x = 0
+init_y = 0
+init_z = 0  # 以base_footprint为整个零件的连体基
+init_theta = 0
+
+class Set_Model_State(Node):
+    global init_x, init_y, init_z, init_theta
+
+    # class to package the pose of obj, (x, y, z, theta)
+    class State(object):
+        def __init__(self, x, y, z, theta):
+            self.x = x
+            self.y = y
+            self.z = z
+            self.theta = theta
+
+    ##################################### initializing ####################################
+    def __init__(self,name):
+        # newly added
+        super().__init__(name)  
+        self.last_t=0
+        self.dt=0
+        self.current_time = rclpy.time.Time()
+        self.state = self.State(init_x, init_y, init_z, init_theta) # 初始xyz
+        self.tf2_broadcaster = StaticTransformBroadcaster(self)
+        self.control_rate=10        # need to be adjusted further according to dt
+        # publish the new position of levitator to gazebo
+        self.state_pub = self.create_publisher('gazebo/set_model_state', ModelState, queue_size=10)
+        # subscribe control input
+        Node.create_subscription (Twist,'/cmd_vel', self.MotionCallback)
+        Node.create_subscription("gazebo/set_model_state", ModelState, self.broadcast_odom_tf)
+    
+    def broadcast_odom_tf(self,model_state):
+        parent_frame = 'odom'
+        child_frame = 'base_footprint'                 
+        time_now = rclpy.Time.now()
+        translation = (model_state.pose.position.x, model_state.pose.position.y, model_state.pose.position.z)
+        rotation = (model_state.pose.orientation.x,model_state.pose.orientation.y,model_state.pose.orientation.z,model_state.pose.orientation.w)
+        # Send the transformation
+        self.tf_broadcaster.sendTransform(translation, rotation, time_now,child_frame,parent_frame)
+
+    # main function
+    # receive the control signal and generate the modelstate, modify the modelstate
+    # @return   NULL
+    def MotionCallback(self,cmd_vel):
+        state_t = ModelState()
+        state_t.model_name = "tribot"
+        self.current_time = rclpy.get_time()
+        self.dt = self.current_time - self.last_t
+        
+        if (self.last_t==0):
+            self.last_t = self.current_time
+            initial_state = self.state
+            initial_state_pub = self.Generate_State(initial_state,"tribot")
+            # rospy.loginfo(initial_state_pub)
+            self.state_pub.publish(initial_state_pub)
+        else :
+            self.last_t = self.current_time
+            self.state = self.forward_dynamics(self.state, cmd_vel)
+            state_t_pub=self.Generate_State(self.state,"tribot")
+            # rospy.loginfo(state_t_pub)
+            self.state_pub.publish(state_t_pub)
+
+    # generate ModelState msg by given pose, which fits gazebo env
+    # @param    pose: given pose
+    #           name: model_name
+    # @return   ModelState()
+    def Generate_State(self, state, name):
+        state_p = ModelState()
+        state_p.model_name = name
+
+        qtn = tf.transformations.quaternion_from_euler(0, 0, state.theta)
+        state_p.pose.orientation.x = qtn[0]
+        state_p.pose.orientation.y = qtn[1]
+        state_p.pose.orientation.z = qtn[2]
+        state_p.pose.orientation.w = qtn[3]
+
+        state_p.pose.position.x = state.x
+        state_p.pose.position.y = state.y
+        state_p.pose.position.z = state.z
+        return state_p
+        
+    # update state by given control signal
+    # @param    state_now: now state， control input
+    # @return   state_next
+    def forward_dynamics(self, state_now, cmd_vel):
+        v = cmd_vel.linear.x
+        omega = cmd_vel.angular.z
+        x_next = state_now.x + v*cos(state_now.theta)*self.dt
+        y_next = state_now.y + v*sin(state_now.theta)*self.dt
+        z_next = state_now.z
+        theta_next = state_now.theta + omega*self.dt
+        # limit theta into the range of (-pi,pi)
+        theta_next = atan2(sin(theta_next), cos(theta_next))
+        state_next = self.State(x_next, y_next, z_next, theta_next)
+        return state_next
+'''       
+if __name__ == '__main__':
+    try:
+        rclpy.init(args='set_kinematic')
+        state_set = Set_Model_State()
+        rclpy.spin()
+    except rclpy.ROSInterruptException:
+        pass
+'''
+def main(args=None):                                 # ROS2节点主入口main函数
+    rclpy.init(args='set_kinematic')                            # ROS2 Python接口初始化
+    node = Set_Model_State("tribot")        # 创建ROS2节点对象并进行初始化
+    rclpy.spin(node)                                 # 循环等待ROS2退出
+    node.destroy_node()                              # 销毁节点对象
+    rclpy.shutdown()                                 # 关闭ROS2 Python接口
---
+++ build/tribot/build/lib/tribot/tribot_teleop.py
@@ -0,0 +1,190 @@
+'''
+FileName: 
+Description:键盘控制 
+Autor: Liujunjie/Aries-441
+StudentNumber: 521021911059
+Date: 2022-11-10 19:20:24
+E-mail: sjtu.liu.jj@gmail.com/sjtu.1518228705@sjtu.edu.cn
+LastEditTime: 2022-11-11 23:24:49
+'''
+
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import rclpy
+from rclpy.node import Node
+from geometry_msgs.msg import Twist
+from rclpy.qos import DurabilityPolicy
+from rclpy.qos import QoSProfile
+from rclpy.qos_event import SubscriptionEventCallbacks
+import sys, select, termios, tty
+import time
+msg = """
+Control tribot!
+---------------------------
+Moving around:
+   u    i    o
+   j    k    l
+   m    ,    .
+
+q/z : increase/decrease max speeds by 10%
+w/x : increase/decrease only linear speed by 10%
+e/c : increase/decrease only angular speed by 10%
+space key, k : force stop
+anything else : stop smoothly
+
+CTRL-C to quit
+"""
+
+moveBindings = {
+        'i':(1,0),
+        'o':(1,-1),
+        'j':(0,1),
+        'l':(0,-1),
+        'u':(1,1),
+        ',':(-1,0),
+        '.':(-1,1),
+        'm':(-1,-1),
+           }
+
+speedBindings={
+        'q':(1.1,1.1),
+        'z':(.9,.9),
+        'w':(1.1,1),
+        'x':(.9,1),
+        'e':(1,1.1),
+        'c':(1,.9),
+          }
+'''
+def getKey():
+    tty.setraw(sys.stdin.fileno())
+    rlist, _, _ = select.select([sys.stdin], [], [], 0.1)
+    if rlist:
+        key = sys.stdin.read(1)
+    else:
+        key = ''
+    termios.tcsetattr(sys.stdin, termios.TCSADRAIN, termios.tcgetattr(sys.stdin))
+    return key
+
+speed = .2
+turn = 1
+
+def vels(speed,turn):
+    return "currently:\tspeed %s\tturn %s " % (speed,turn)
+
+#settings = termios.tcgetattr(sys.stdin)
+'''
+class Keyboard(Node):
+    def _init_(self, name):
+        super()._init_(name)
+        pub = self.create_publisher(Twist,'cmd_vel',10)
+        self.timer=self.create_timer(0.1, self.timer_callback)
+    def timer_callback(self):
+        x = 0
+        th = 0
+        status = 0
+        count = 0
+        acc = 0.1
+        target_speed = 0
+        target_turn = 0
+        control_speed = 0
+        control_turn = 0
+        try:
+            print(msg)
+            print(self.vels(speed,turn))
+            while(1):
+                key = self.getKey()
+                # 运动控制方向键（1：正方向，-1负方向）
+                if key in moveBindings.keys():
+                    x = moveBindings[key][0]
+                    th = moveBindings[key][1]
+                    count = 0
+                # 速度修改键
+                elif key in speedBindings.keys():
+                    speed = speed * speedBindings[key][0]  # 线速度增加0.1倍
+                    turn = turn * speedBindings[key][1]    # 角速度增加0.1倍
+                    count = 0
+
+                    print(self.vels(speed,turn))
+                    if (status == 14):
+                        print(msg)
+                    status = (status + 1) % 15
+                # 停止键
+                elif key == ' ' or key == 'k' :
+                    x = 0
+                    th = 0
+                    control_speed = 0
+                    control_turn = 0
+                else:
+                    count = count + 1
+                    if count > 4:
+                        x = 0
+                        th = 0
+                    if (key == '\x03'):
+                        break
+
+                # 目标速度=速度值*方向值
+                target_speed = speed * x
+                target_turn = turn * th
+
+                # 速度限位，防止速度增减过快
+                if target_speed > control_speed:
+                    control_speed = min( target_speed, control_speed + 0.02 )
+                elif target_speed < control_speed:
+                    control_speed = max( target_speed, control_speed - 0.02 )
+                else:
+                    control_speed = target_speed
+
+                if target_turn > control_turn:
+                    control_turn = min( target_turn, control_turn + 0.1 )
+                elif target_turn < control_turn:
+                    control_turn = max( target_turn, control_turn - 0.1 )
+                else:
+                    control_turn = target_turn
+
+                # 创建并发布twist消息
+                twist = Twist()
+                twist.linear.x = control_speed; 
+                twist.linear.y = 0; 
+                twist.linear.z = 0
+                twist.angular.x = 0; 
+                twist.angular.y = 0; 
+                twist.angular.z = control_turn
+                self.pub.publish(twist)
+
+        except:
+            print(1,1.1)
+
+        finally:
+            twist = Twist()
+            twist.linear.x = 0.0; twist.linear.y = 0.0; twist.linear.z = 0.0
+            twist.angular.x = 0.0; twist.angular.y = 0.0; twist.angular.z = 0.0
+            self.pub.publish(twist)
+
+        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, termios.tcgetattr(sys.stdin))
+
+    def getKey():
+        tty.setraw(sys.stdin.fileno())
+        rlist, _, _ = select.select([sys.stdin], [], [], 0.1)
+        if rlist:
+            key = sys.stdin.read(1)
+        else:
+            key = ''
+        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, termios.tcgetattr(sys.stdin))
+        return key
+
+    speed = .2
+    turn = 1
+
+    def vels(speed,turn):
+        return "currently:\tspeed %s\tturn %s " % (speed,turn)
+
+def main(args=None):                                 # ROS2节点主入口main函数
+    rclpy.init(args=args)                            # ROS2 Python接口初始化
+    node = Keyboard("tribot")        # 创建ROS2节点对象并进行初始化
+    while rclpy.ok():                            # ROS2系统是否正常运行
+        node.get_logger().info("Hello World")    # ROS2日志输出
+        time.sleep(0.5)                          # 休眠控制循环时间  
+    rclpy.spin(node)                                 # 循环等待ROS2退出
+    node.destroy_node()                              # 销毁节点对象
+    rclpy.shutdown()                                 # 关闭ROS2 Python接口
